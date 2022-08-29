9e7c75cadf1dc1159e7291c3a3983c4a95d14fee
Konee <104918313+KoneeVibes@users.noreply.github.com>
2022-08-29 04:19:31 -0500

    working on login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,31 +5,52 @@ import LeftBg from '../assets/Login/LeftBg.svg'
 const Styledwrapper = styled.div`
 background: #FFFFFF;
 height: 100vh;
+overflow: hidden;
 display: flex;
 align-items: center;
-color: #000;
+color: #00302EE8;
 
-.form{
+
+.form-flex-box{
   display: flex;
+  flex: 1;
   flex-direction: column;
   align-items: center;
+  margin-top: 3em;
 }
 
 .image-flex-box{
-  height: 100%;
+  flex: 1;
 }
 
-// .form-header{
-//   flex: 0.2;
-// }
+.bottom-flex{
+  display: flex;
+  align-items: center;
+}
 
-.form-header h2{
+.form-flex-box input{
+  // padding: 2.5em 30.5em 1.35em 2.5em;
+  width: 30.5em;
+  height: 4.4375em;
+  margin: 1.5em;
+  border: 1px solid #FBDDBB82;
+  border-radius: 5px;
+}
+
+#login-button{
+  // width: 31em;
+  color: #FBDDBB;
+  background: #00302E;
   text-align: center;
 }
 
-// .form-content{
-//   flex: 0.8;
-// }
+.margin-right-p{
+  margin-right: 11em;
+}
+
+.margin-left-p{
+  margin-left: 9em;
+}
 `
 
 const Login = () => {
@@ -38,13 +59,16 @@ const Login = () => {
       <div className='image-flex-box'>
         <img src={LeftBg} alt='Food' height='100%'/>
       </div>
-      <div className='form'>
-        <div className='form-header'>
-          <h2>Welcome Back!</h2>
-        </div>
-        <div className='form-content'>
-          <h2>Welcome Back!</h2>
-
+      <div className='form-flex-box'>
+        <h2>Welcome Back!</h2>
+        <form>
+           <input type='email' placeholder='Your Email address'/><br/>
+           <input type='password' placeholder='Your Password'/><br/>
+           <input type="submit" value="LOGIN" id='login-button'></input>
+        </form>
+        <div className='bottom-flex'>
+          <div><p className='margin-right-p'>Create an account</p></div> 
+          <div><p className='margin-left-p'>Forgot Password</p></div> 
         </div>
       </div>
 
